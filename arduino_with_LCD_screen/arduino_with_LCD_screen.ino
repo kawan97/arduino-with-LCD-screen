2ed5fa4fd5041b1891218b6bec1b8747a0c32e26
kawan97 <pshtiwankawan@gmail.com>
2022-05-05 07:58:24 -0700

    add some extra code

--- arduino_with_LCD_screen/arduino_with_LCD_screen.ino
+++ arduino_with_LCD_screen/arduino_with_LCD_screen.ino
@@ -1,5 +1,5 @@
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 20,4);
+LiquidCrystal_I2C lcd(0x27, 16,2);
 
 void setup() {
   lcd.init();
@@ -7,15 +7,32 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("hello world");
   lcd.setCursor(15, 0);
-  lcd.print("1");
+  lcd.print("15656");
    lcd.setCursor(0,1);
   lcd.print("kawa");
 }
 
 void loop() {
-  delay(5000);
-   lcd.clear();
-   lcd.print("slaw mn kawanm");
+//  delay(1000);
+//   lcd.clear();
+//   lcd.print("slaw mn kawanm");
+//   delay(1000);
+
+//  lcd.autoscroll();
+
+  lcd.scrollDisplayLeft();
+  delay(100);
+
+
+//  lcd.cursor();
+//  delay(500);
+ lcd.noCursor();
+//  delay(500);
+
+//  lcd.blink();
+//  delay(5000);
+  lcd.noBlink();
+//  delay(2000);
 
 
 }
